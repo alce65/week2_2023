92848c58df867e9d870fe61cc42d6890523e5574
Alejandro Cerezo <alce65@hotmail.es>
2023-01-25 17:15:40 +0100

    Add TDD factorial

---
+++ Day3/array.js
@@ -0,0 +1,14 @@
+const aData = [1, 2, 3, 4, 5, 'Pepe'];
+console.log(aData);
+console.log(aData.length);
+aData.indexOf(4);
+
+//  Declaración
+
+function foo1() {}
+
+//  Asignación
+
+const foo2 = function () {};
+foo1();
+foo2();
