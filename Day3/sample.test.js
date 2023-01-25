439d4a103e9295264cfa6ae713d110e1a685b067
Alejandro Cerezo <alce65@hotmail.es>
2023-01-25 14:02:58 +0100

    Add test with loop of cases

--- Day3/sample.test.js
+++ Day3/sample.test.js
@@ -1,8 +1,26 @@
 import { add } from './sample';
 
-describe('Testing Add', () => {
-  test(' If a = 1 and b = 1 result should be 2', () => {
-    const r = add(1, 1);
-    expect(r).toBe(2);
+describe('Given Add function', () => {
+  describe('When we have two numbers', () => {
+    test('Then If a = 1 and b = 1 result should be 2', () => {
+      // Arrange
+      const a = 1;
+      const b = 1;
+      const expected = 2;
+      // Act
+      const r = add(a, b);
+      // Assert
+      expect(r).toBe(expected);
+    });
+    test('Then If a = NaN and b = NaN result should be false', () => {
+      // Arrange
+      const a = 1;
+      const b = 1;
+      const expected = 2;
+      // Act
+      const r = add(a, b);
+      // Assert
+      expect(r).toBe(expected);
+    });
   });
 });
