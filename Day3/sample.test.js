0a9334352b1eda5ce6d4c935d4faa9f526778e76
Alejandro Cerezo <alce65@hotmail.es>
2023-01-25 12:52:21 +0100

    Update jest configuration

--- Day3/sample.test.js
+++ Day3/sample.test.js
@@ -1,6 +1,8 @@
-describe('Testing Add', () => {
-  test('should first', () => {
+import { add } from './sample';
 
-    expect(x).toBe)
-   })
+describe('Testing Add', () => {
+  test(' If a = 1 and b = 1 result should be 2', () => {
+    const r = add(1, 1);
+    expect(r).toBe(2);
+  });
 });
